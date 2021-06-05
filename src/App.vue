07565e1ed2352e6835bb1169fda83218f02f50a6
Anthony <spartanz51@live.fr>
2021-06-05 13:53:42 +0200

    bug fix registration

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,31 @@
 <template>
   <div id="app">
-    <img id="logo" alt="Nosta logo" src="./assets/logo.png">
-    <MemberRegistration
-      influencer="Nehalia"
-      address="0x2C3d88B26728b0b975291FF19b6eFAC0388E9fB9"
-      contract="0x6f92b08b24010da5dd0a9c65a536cc8da1c69d1d"
-    />
+    <router-view/>
   </div>
 </template>
 
-<script>
-import MemberRegistration from './components/MemberRegistration.vue'
-
-export default {
-  name: 'App',
-  components: {
-    MemberRegistration
-  }
-}
-</script>
-
 <style>
-
-#logo {
-  width: 300px;
-}
-
 body {
-  background-color: black;
+    background-color: black;
 }
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img id="logo" alt="Nosta logo" src="../assets/logo.png">
+    <MemberRegistration
+      :influencer="$route.params.influencerName || 'Nehaliah'"
+      :address="$route.params.influencerId || '0x2C3d88B26728b0b975291FF19b6eFAC0388E9fB9'"
+      contract="0x6f92b08b24010da5dd0a9c65a536cc8da1c69d1d"
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MemberRegistration from '../components/MemberRegistration.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    MemberRegistration
+  }
+}
+</script>
+
+<style scoped>
+#logo {
+  width: 300px;
+}
+</style>
